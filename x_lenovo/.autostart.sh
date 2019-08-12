54e38c86de605e51cbe56146e0386c882ac8580f
Afonso Ribeiro <afonso.viegas.0@gmail.com>
2019-08-12 13:23:08 +0100

    laptop support

---
+++ x_lenovo/.autostart.sh
@@ -0,0 +1,11 @@
+# Statusbar loop
+#while true; do
+#   xsetroot -name "$(date +%d && date +%B\ %A && echo '|' && date +%H:%M)"
+#   sleep 1m    # Update time every minute
+#done &
+# programs
+exec python /home/bliss/lenovo-miix-320/auto_rotate.py &
+exec setxkbmap pt &
+exec start-pulseaudio-x11 &
+exec sxhkd &
+sleep 2s && nitrogen --random --set-scaled ~/wallpapers/
