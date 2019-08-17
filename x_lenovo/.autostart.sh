34d7136b25e4e4a1bd41865c3c8367914b98a0ea
Afonso Ribeiro <afonso.viegas.0@gmail.com>
2019-08-17 11:41:11 +0100

    more emacs features

--- x_lenovo/.autostart.sh
+++ x_lenovo/.autostart.sh
@@ -8,4 +8,5 @@ exec python /home/bliss/lenovo-miix-320/auto_rotate.py &
 exec setxkbmap pt &
 exec start-pulseaudio-x11 &
 exec sxhkd &
-sleep 2s && nitrogen --random --set-scaled ~/wallpapers/
+sleep 2.7s && nitrogen --random --set-scaled ~/wallpapers/ &
+exec setxkbmap -option ctrl:nocaps
