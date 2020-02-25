f78c7143acd9260b2deb7c4757a061895252814a
Blisszs <afonso.viegas.0@gmail.com>
2020-02-25 18:24:30 +0000

    cleanup

--- x/.autostart.sh
+++ x/.autostart.sh
@@ -1,11 +1,4 @@
-# Statusbar loop
-#while true; do
-#   xsetroot -name "$(date +%d && date +%B\ %A && echo '|' && date +%H:%M)"
-#   sleep 1m    # Update time every minute
-#done &
-
-# programs
-exec feh --bg-fill -z wallpapers &
-exec setxkbmap pt &
-exec pulseaudio &
-exec sxhkd
+feh --bg-fill -z wallpapers &
+setxkbmap pt &
+pulseaudio &
+sxhkd
